448c2fab725fdc51f3f7727e26ccee777eab44be
小麦 <82826006+xuzeznb@users.noreply.github.com>
2022-06-01 01:18:21 +0800

    Delete src/main/java/com/example/demo directory

--- src/main/java/com/example/demo/config/ces.java
+++
@@ -1,7 +0,0 @@
-//package com.example.demo.Controller;
-//
-//public class ces {
-//    public static void main(String args[]) {
-//        System.out.println(System.getProperty("user.dir") + "\\file" + "\\4df2f2d537a446d9af15b4ce32b66c1c.png");
-//    }
-//}
